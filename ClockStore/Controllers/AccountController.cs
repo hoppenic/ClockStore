65b0225d8977f4abd1a946079a4a5bb0a4eb5d1d
hoppenic <hoppenic@gmail.com>
2018-09-26 11:30:53 -0500

    updates

---
+++ ClockStore/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ClockStore.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ClockStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        //responds on GET //Account/Register
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //responds on POST /Account/Register
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(string username, string password)
+        {
+            return Ok();
+        }
+    }
+}
